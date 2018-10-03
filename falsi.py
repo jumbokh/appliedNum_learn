485f29ac23db5716b7dccc842d2df1e8bb42c79f
jumbokh <jumbokh@gmail.com>
2018-10-04 01:15:08 +0800

    recovery old program

---
+++ falsi.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+## module falsi
+''' root = ridder(f,a,b,tol=1.0e-9).
+    Finds a root of f(x) = 0 with rule of false position method.
+    The root must be bracketed in (a,b).
+'''
+import error
+import math
+from numpy import sign
+
+def falsi(f,a,b,tol=1.0e-9):   
+    fa = f(a)
+    if fa == 0.0: return a
+    fb = f(b)
+    if fb == 0.0: return b
+    if sign(fa) == sign(fb): error.err('Root is not bracketed')
+    for i in range(30):
+      # Compute the improved root x from falsi formula
+        
+        s = b - fb*(b-a)/(fb-fa)
+        fx = f(s)
+        print(i+1,'{0:8.6f}'.format(a),'{0:8.6f}'.format(b),'{0:8.6f}'.format(s),'{0:8.6f}'.format(fx))
+        if s == 0.0: return None
+        
+      # Test for convergence
+        if i > 0:
+            if abs(fx) < tol:
+             print('regula falsi has coverged')
+             return s
+        
+      # Re-bracket the root as tightly as possible
+        if sign(fx) != sign(fa): 
+             b = s; fb = fx
+        else: a = s; fa = fx
+            
+    return None
+    print('Too many iterations')
+
